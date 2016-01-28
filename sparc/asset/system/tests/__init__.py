17cd48fa122f89d9394e8a995a5d894ec34c6f26
David Davis <davisd50@gmail.com>
2016-01-28 15:25:48 -0500

    update tests for new sparc.configuration.zcml.Configure implementation that retrieves vocabularies from the global registry

--- sparc/asset/system/tests/__init__.py
+++ sparc/asset/system/tests/__init__.py
@@ -1,8 +1,11 @@
-from zope.schema.vocabulary import getVocabularyRegistry
+from zope.component import getGlobalSiteManager
+from zope.interface import alsoProvides
+from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary
 
 def register_system_types_vocab_from_values(values=['test']):
     """Convenience function for vocab registration"""
     vocab_factory = lambda x: SimpleVocabulary.fromValues(values)
-    vr = getVocabularyRegistry()
-    vr.register(u'sparc.asset.system.types', vocab_factory)
+    alsoProvides(vocab_factory, IVocabularyFactory)
+    gsm = getGlobalSiteManager()
+    gsm.registerUtility(vocab_factory, IVocabularyFactory, u'sparc.asset.system.types')
